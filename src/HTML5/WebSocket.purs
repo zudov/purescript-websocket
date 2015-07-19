e138a672ca826fa7d0c7f0685565c584bdb84c1d
Konstantin Zudov <konstantin@anche.no>
2015-07-19 03:10:08 +0300

    Ported to 0.7

--- src/HTML5/WebSocket.purs
+++ src/HTML5/WebSocket.purs
@@ -14,6 +14,7 @@ module HTML5.WebSocket
   , runWebSocket
   ) where
 
+import Prelude
 import Control.Monad.Eff (Eff (..))
 import Data.Either
 import Data.Function
@@ -22,7 +23,7 @@ import Control.Monad.Cont.Trans
 
 type WebSocketConfig =
   { uri :: URI
-  , protocols :: [Protocol]
+  , protocols :: Array Protocol
   }
 
 type URI = String
@@ -44,41 +45,13 @@ type WebSocketHandler eff = Socket ->
 runWebSocket :: forall eff. WebSocket eff Unit -> WithWebSocket eff Unit
 runWebSocket = flip runContT return
 
-foreign import withWebSocketImpl """
-  function withWebSocketImpl (config, handlers, ok, err) {
-    return function () {
-      var socket, h;
-      try {
-        socket = new window.WebSocket(config.uri, config.protocols);
-      } catch (e) {
-        err(e.type)();
-        return {};
-      }
-      h = handlers(socket);
-      socket.onopen = function () {
-        h.onOpen();
-        return {};
-      };
-      socket.onmessage = function (ev) {
-        h.onMessage(ev.data)();
-        return {};
-      };
-      socket.onclose = function () {
-        ok({})();
-        return {};
-      };
-      socket.onerror = function (e) {
-        err(e)();
-        return {};
-      };
-      return {};
-    };
-  }""" :: forall eff.
-          Fn4 WebSocketConfig
-              (WebSocketHandler eff)
-              (Unit -> WithWebSocket eff Unit)
-              (WebSocketError -> WithWebSocket eff Unit)
-              (WithWebSocket eff Unit)
+foreign import withWebSocketImpl
+                :: forall eff.
+                  Fn4 WebSocketConfig
+                      (WebSocketHandler eff)
+                      (Unit -> WithWebSocket eff Unit)
+                      (WebSocketError -> WithWebSocket eff Unit)
+                      (WithWebSocket eff Unit)
 
 defaultHandlers :: forall eff. WebSocketHandler eff
 defaultHandlers _ =
@@ -89,17 +62,11 @@ defaultHandlers _ =
 withWebSocket :: forall eff. WebSocketConfig
                  -> WebSocketHandler eff
                  -> WebSocket eff (Either WebSocketError Unit)
-withWebSocket c h = ContT $ \k -> runFn4 withWebSocketImpl c h
-                                         (k <<< Right)
-                                         (k <<< Left)
+withWebSocket c h = ContT $ \k -> runFn4 withWebSocketImpl c h (k <<< Right)
+                                                               (k <<< Left)
 
-foreign import sendImpl """
-  function sendImpl (socket, data) {
-    return function () {
-      socket.send(data);
-      return {};
-    };
-  }""" :: forall eff.  Fn2 Socket String (WithWebSocket eff Unit)
+foreign import sendImpl
+                :: forall eff. Fn2 Socket String (WithWebSocket eff Unit)
 
-send :: forall eff.  Socket -> String -> WithWebSocket eff Unit
+send :: forall eff. Socket -> String -> WithWebSocket eff Unit
 send = runFn2 sendImpl
